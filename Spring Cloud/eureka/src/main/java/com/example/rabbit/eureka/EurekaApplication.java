f1e76f61c13f194b06adca94aa7fed95ac04d82a
zhengzhoutao <zhengzhoutao@ancun.com>
2018-05-11 17:33:05 +0800

    gradle骨架

---
+++ Spring Cloud/eureka/src/main/java/com/example/rabbit/eureka/EurekaApplication.java
@@ -0,0 +1,13 @@
+package com.example.rabbit.eureka;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
