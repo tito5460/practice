cf4405e6cb73e9ea707cbc09e413fc89492413c2
zhengzhoutao <zhengzhoutao@ancun.com>
2018-05-18 18:26:33 +0800

    config-server

---
+++ spring-cloud/config-client/src/main/java/com/tito/config/client/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.tito.config.client.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/h")
+public class HelloController {
+
+	@Value("${server.port}")
+	private String port;
+
+	@RequestMapping("/hello")
+	public ResponseEntity<?> hello() {
+		String str = "Hello, this is eureka client.port:" + port;
+		return ResponseEntity.ok(str);
+	}
+
+}
