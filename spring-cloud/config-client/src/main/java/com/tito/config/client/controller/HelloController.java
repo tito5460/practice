ee7438812227416f472e9df98e0c142be5901f4c
Tito <tito5460@github.com>
2018-05-22 17:59:00 +0800

    spring-cloud-bus

--- spring-cloud/config-client/src/main/java/com/tito/config/client/controller/HelloController.java
+++ spring-cloud/config-client/src/main/java/com/tito/config/client/controller/HelloController.java
@@ -1,10 +1,12 @@
 package com.tito.config.client.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RefreshScope//要想刷新spring-cloud-config，必须加这个注解
 @Controller
 @RequestMapping("/h")
 public class HelloController {
